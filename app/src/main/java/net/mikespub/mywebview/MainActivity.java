1c63666ac5e47933765fc370e10df732213010e6
mikespub <github@mikespub.net>
2020-02-01 13:17:22 +0100

    Initial commit

---
+++ app/src/main/java/net/mikespub/mywebview/MainActivity.java
@@ -0,0 +1,13 @@
+package net.mikespub.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
