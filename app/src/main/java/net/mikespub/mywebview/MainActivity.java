3d7aae6603a14cdbf36335867ad4dacc3e7e7c15
mikespub <github@mikespub.net>
2020-02-01 17:39:52 +0100

    clean up & release

--- app/src/main/java/net/mikespub/mywebview/MainActivity.java
+++ app/src/main/java/net/mikespub/mywebview/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myWebView = (WebView) findViewById(R.id.activity_main_webview);
+        myWebView = findViewById(R.id.activity_main_webview);
         // Enable Javascript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
