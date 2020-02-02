b62fab744c7cda4366ceee1869f866a61048e3ce
mikespub <github@mikespub.net>
2020-02-02 03:00:19 +0100

    skip handling epub files

--- app/src/main/java/net/mikespub/mywebview/MyAppWebViewClient.java
+++ app/src/main/java/net/mikespub/mywebview/MyAppWebViewClient.java
@@ -18,6 +18,37 @@ public class MyAppWebViewClient extends WebViewClient {
             String[] myHostEndsArr = view.getContext().getResources().getStringArray(R.array.urlhost_endswith);
             for (String s : myHostEndsArr) {
                 if (host.endsWith(s)) {
+                    // skip epub files - TODO: save in media directory https://developer.android.com/training/data-storage/app-specific#media ?
+                    // if (!path.endsWith(".epub") && (query == null || !query.contains("type=epub"))) {
+                    //     return false;
+                    // }
+                    boolean isMatch = false;
+                    final String path = uri.getPath();
+                    if (path != null) {
+                        String[] myPathNotEndsArr = view.getContext().getResources().getStringArray(R.array.urlpath_not_endswith);
+                        for (String t : myPathNotEndsArr) {
+                            if (path.endsWith(t)) {
+                                isMatch = true;
+                                break;
+                            }
+                        }
+                        if (isMatch) {
+                            continue;
+                        }
+                    }
+                    final String query = uri.getQuery();
+                    if (query != null) {
+                        String[] myQueryNotContainsArr = view.getContext().getResources().getStringArray(R.array.urlquery_not_contains);
+                        for (String q : myQueryNotContainsArr) {
+                            if (query.contains(q)) {
+                                isMatch = true;
+                                break;
+                            }
+                        }
+                        if (isMatch) {
+                            continue;
+                        }
+                    }
                     return false;
                 }
             }
